b452d87a0ecc1b969ed73d768607fe33e0eae83d
Lsan <Lsan>
2016-09-09 16:46:27 +0200

    test commit
    test desc

---
+++ breadcrumbsService.js
@@ -0,0 +1,72 @@
+(function() {
+    'use strict';
+    angular.module("services").service("breadcrumbsService", BreadcrumbsService);
+
+    BreadcrumbsService.$inject = ["$window"];
+
+    function BreadcrumbsService($window) {
+        var me = this;
+        me.breadcrumbsToAdd = [];
+        me.breadcrumbsToAdd.push({
+            left: {
+                icon: "crosshairs",
+                description: "Start",
+            },
+            right: {
+                icon: "briefcase",
+                description: "Accounts",
+            },
+            actionOnClick: log
+        }, {
+            left: {
+                icon: "filter"
+            },
+            operator: "AND",
+            right: {
+                title: "Set: Accounts",
+                descriptions: {
+                    first: "Type: Business",
+                    second: "Age: from 37 years to 57 years"
+                }
+            },
+            actionOnClick: popup
+        }, {
+            left: {
+                icon: "expand",
+            },
+            right: {
+                title: "Connection: Accounts - Transactions",
+                descriptions: {
+                    first: "Type: Remitter, Beneficiary, Owners, ...",
+                    second: "Value: >=2"
+                }
+            },
+            actionOnClick: redirect
+        });
+
+
+        function getBreadcrumb() {
+            if (me.breadcrumbsToAdd.length) {
+                return me.breadcrumbsToAdd.shift();
+            }
+        }
+
+        function log() {
+            console.log("breadcrumb was clicked!");
+        }
+
+        function popup() {
+            alert("breadcrumb was clicked!");
+        }
+
+        function redirect() {
+            $window.location.href = 'http://www.google.com';
+        }
+
+        return {
+            getBreadcrumb: getBreadcrumb
+        }
+
+
+    }
+})();
